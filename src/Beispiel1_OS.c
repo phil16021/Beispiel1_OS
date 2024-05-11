15484ead1c0d84996bda118684b43923f8f7668b
philipp <ko.philipp@gmx.at>
2024-05-12 00:38:13 +0200

    Tabellen

--- src/Beispiel1_OS.c
+++ src/Beispiel1_OS.c
@@ -14,37 +14,13 @@
 #include "Beispiel1_OS.h"
 
 int main(void) {
-	/* Deklaration, teilweise mit Initialisierung */ 
-	/* Startwerte */ 
-	int anzahl, nummer, nochEiner;
-	double preis, summeRechnung;
-	/* Startwerte */ 
-	nummer = 0;
-	summeRechnung = 0.0;
-	/* Abfrage */ 
-	printf("Wollen Sie Artikel eingeben (Ja=1, Nein=0): ");
-	scanf("%d", &nochEiner);
-	/* Bedingte Wiederholung, Prüfung vorher */ 
-	while(nochEiner == 1)
-	{
-	/* Laufende Nummer erhöhen */ 
-	nummer = nummer + 1;
-	/* Eingabe */ 
-	printf("Artikel %d, Anzahl: ", nummer);
-	scanf("%d", &anzahl);
-	printf("Artikel %d, Preis in Euro: ", nummer);
-	scanf("%lf", &preis);
-	/* Berechnung */ 
-	summeRechnung = summeRechnung + anzahl * preis;
-	/* Abfrage */ 
-	printf("Noch ein Artikel (Ja=1, Nein=0): ");
-	scanf("%d", &nochEiner);
+	double einzelpreis = 1.45;
+
+	printf("Anzahl        Preis\n");
+	for(uint32_t i = 0; i < 10; i++) {
+		printf("%6d", i+1);
+		printf("%8.2f Euro\n",einzelpreis * (i + 1));
 	}
-	/* Ausgabe */ 
-	if(nummer == 0)
-	printf("Keine Eingabe\n");
-	else 
-	printf("Summe der Rechnung: %.2f Euro\n", summeRechnung);
 
 	return EXIT_SUCCESS;
 }
