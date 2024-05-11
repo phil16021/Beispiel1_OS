87556b42f4e53ed917422c639727cbf76de5f96c
philipp <ko.philipp@gmx.at>
2024-05-11 23:21:08 +0200

    Uebungsaufgabe Verzweigung

--- src/Beispiel1_OS.c
+++ src/Beispiel1_OS.c
@@ -19,8 +19,7 @@ int main(void) {
 	double* preisArtikel = (double*) calloc(2, sizeof(double));
 	double summeRechnung = 0, summeRabatt = 0;
 	double preis = 1.85;
-	uint32_t anzahlArtikelHelpline[] = {5, 2};	
-	double preisArtikelHelpline[] = {2.1, 1.6};
+
 	/* Eingabe */ 
 	// for(int i = 0; i < 2; i++) {
 	// 	printf("%d. Artikel: ", i+1);
@@ -28,8 +27,11 @@ int main(void) {
 	// 	printf("Preis in Euro: ");
 	// 	scanf("%lf", preisArtikel + i);
 	// }
-	anzahlArtikel = anzahlArtikelHelpline;	
-	preisArtikel = preisArtikelHelpline;
+	*anzahlArtikel = 5;	
+	*(anzahlArtikel+1) = 2;
+	*preisArtikel = 2.1;
+	*(preisArtikel+1) = 1.6;
+
 
 	/* Verarbeitung */
 	for(int i = 0; i < 2; i++) {
@@ -41,57 +43,27 @@ int main(void) {
 	printf("Summe ohne Rabatt: %.2f Euro\n", summeRechnung);
 	printf("Summe mit Rabatt: %.2f Euro\n", summeRabatt);
 
+	free(anzahlArtikel);
+	free(preisArtikel);
 	/* Bedingte Ausführung eines Blocks von Anweisungen */ 
-	if(preis > 0.99)
-	{
-		printf("Ein teurer Artikel\n");
-		printf("Brauchen wir den wirklich?\n");
-	}
-	/* Bedingte Ausführung einer einzelnen Anweisung */ 
-	if(preis < 1.0)
-		printf("Ein billiger Artikel\n");
 
-	// Vergleichsbeispiel
-	double preisApfel = 1.45, preisBirne = 0.85, preisKiwi = 1.45;
-	/* Größer */ 
-	if(preisApfel > preisBirne)
-	printf("Apfel ist teurer als Birne\n");
-	/* Kleiner */ 
-	if(preisBirne < preisKiwi)
-	printf("Birne ist billiger als Kiwi\n");
-	/* Größer oder gleich */ 
-	if(preisApfel >= preisKiwi)
-	printf("Apfel ist mindestens so teuer wie Kiwi\n");
-	/* Kleiner oder gleich */ 
-	if(preisBirne <= preisKiwi)
-	printf("Birne ist hoechstens so teuer wie Kiwi\n");
-	/* Gleich */ 
-	if(preisApfel == preisKiwi)
-	printf("Apfel und Kiwi haben denselben Preis\n");
-	/* Ungleich */ 
-	if(preisBirne != preisKiwi)
-	printf("Birne und Kiwi haben unterschiedliche Preise\n");
 
-	/* Falls Bedingung zutrifft */ 
-	if(preis > 0.99)
-		printf("Ein teurer Artikel\n");
-	/* Ansonsten, falls also Bedingung nicht zutrifft */ 
-	else 
-	{
-		printf("Ein billiger Artikel\n");
-		printf("Den nehmen wir\n");
+	uint32_t anzahlEingekaufteArtikel = 0;
+	double preisEingekaufteArtikel = 0;
+
+	printf("Anzahl der eingekauften Artikel: ");
+	scanf("%u", &anzahlEingekaufteArtikel);
+	printf("Preis der eingekauften Artikel: ");
+	scanf("%lf", &preisEingekaufteArtikel);
+
+	summeRechnung = anzahlEingekaufteArtikel * preisEingekaufteArtikel;
+
+	if(summeRechnung > 10) {
+		printf("Summe mit Rabatt: %.2f Euro\n", summeRechnung * (100 - RABATT) / 100);
+	} else {
+		printf("Summe ohne Rabatt: %.2f Euro\n", summeRechnung);
 	}
 
-	double preisBanane = 0.75;
-	/* Logisches Und */ 
-	if(preisBirne < 1.0 && preisBanane < 1.0)
-		printf("Beide Artikel sind billig\n");
-	/* Logisches Oder */ 
-	if(preisApfel > 0.99 || preisBirne > 0.99)
-		printf("Mindestens einer der Artikel ist teuer\n");
-	/* Logisches Nicht */ 
-	if(!(preisBanane > 0.99))
-		printf("Artikel ist nicht teuer\n");
 		
 
 	return EXIT_SUCCESS;
