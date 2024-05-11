e2555950b4e6369ee5eb963f58626ed1438e43f7
philipp <ko.philipp@gmx.at>
2024-05-11 23:09:10 +0200

    Vergleichsoperatoren

--- src/Beispiel1_OS.c
+++ src/Beispiel1_OS.c
@@ -18,14 +18,19 @@ int main(void) {
 	uint32_t* anzahlArtikel = (uint32_t*) calloc(2, sizeof(uint32_t));
 	double* preisArtikel = (double*) calloc(2, sizeof(double));
 	double summeRechnung = 0, summeRabatt = 0;
-
+	double preis = 0.85;
+	uint32_t anzahlArtikelHelpline[] = {5, 2};	
+	double preisArtikelHelpline[] = {2.1, 1.6};
 	/* Eingabe */ 
-	for(int i = 0; i < 2; i++) {
-		printf("%d. Artikel: ", i+1);
-		scanf("%u", anzahlArtikel + i);
-		printf("Preis in Euro: ");
-		scanf("%lf", preisArtikel + i);
-	}
+	// for(int i = 0; i < 2; i++) {
+	// 	printf("%d. Artikel: ", i+1);
+	// 	scanf("%u", anzahlArtikel + i);
+	// 	printf("Preis in Euro: ");
+	// 	scanf("%lf", preisArtikel + i);
+	// }
+	anzahlArtikel = anzahlArtikelHelpline;	
+	preisArtikel = preisArtikelHelpline;
+
 	/* Verarbeitung */
 	for(int i = 0; i < 2; i++) {
 		summeRechnung += *(anzahlArtikel+i) * *(preisArtikel+i);
@@ -35,6 +40,39 @@ int main(void) {
 	/* Ausgabe */ 
 	printf("Summe ohne Rabatt: %.2f Euro\n", summeRechnung);
 	printf("Summe mit Rabatt: %.2f Euro\n", summeRabatt);
+
+	/* Bedingte Ausführung eines Blocks von Anweisungen */ 
+	if(preis > 0.99)
+	{
+		printf("Ein teurer Artikel\n");
+		printf("Brauchen wir den wirklich?\n");
+	}
+	/* Bedingte Ausführung einer einzelnen Anweisung */ 
+	if(preis < 1.0)
+		printf("Ein billiger Artikel\n");
+
+	// Vergleichsbeispiel
+	double preisApfel = 1.45, preisBirne = 0.85, preisKiwi = 1.45;
+	/* Größer */ 
+	if(preisApfel > preisBirne)
+	printf("Apfel ist teurer als Birne\n");
+	/* Kleiner */ 
+	if(preisBirne < preisKiwi)
+	printf("Birne ist billiger als Kiwi\n");
+	/* Größer oder gleich */ 
+	if(preisApfel >= preisKiwi)
+	printf("Apfel ist mindestens so teuer wie Kiwi\n");
+	/* Kleiner oder gleich */ 
+	if(preisBirne <= preisKiwi)
+	printf("Birne ist hoechstens so teuer wie Kiwi\n");
+	/* Gleich */ 
+	if(preisApfel == preisKiwi)
+	printf("Apfel und Kiwi haben denselben Preis\n");
+	/* Ungleich */ 
+	if(preisBirne != preisKiwi)
+	printf("Birne und Kiwi haben unterschiedliche Preise\n");
+
+	
 
 	return EXIT_SUCCESS;
 }
