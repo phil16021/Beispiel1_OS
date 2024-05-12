6fcd7573a7bec431497f814bd3dd4464b133c6cc
philipp <ko.philipp@gmx.at>
2024-05-12 09:11:54 +0200

    Felder

--- src/Beispiel1_OS.c
+++ src/Beispiel1_OS.c
@@ -13,37 +13,43 @@
 #include <stdlib.h>
 #include "Beispiel1_OS.h"
 
-int main(void) {
-	int32_t anzahlArtikel = 0;
-	double preis = 0.0;
+void meinLogo();
+void AusgebenGesamt(double);
 
-	do
-	{
-		printf("Anzahl eingeben:  ");
-		scanf("%d", &anzahlArtikel);
-		if(anzahlArtikel < 0)
-		{
-			printf("Anzahl muss positiv sein\n");
-		}
-	} while (anzahlArtikel < 0);
+void meinLogo()
+{
+	printf("****************************************\n");
+	printf("** Dieses Programm ist von Triz Maker **\n");
+	printf("****************************************\n");
+}
 
-	do
-	{
-		printf("Preis in Euro eingeben: ");
-		scanf("%lf", &preis);
-		if(preis < 0)
-		{
-			printf("Preis muss positiv sein\n");
-		}
-		if (preis > 10)
-		{
-			printf("Preis muss kleiner als 10 Euro sein\n");
-		}
-		
-	} while (preis < 0 || preis > 10);
+/* Definition einer Funktion */ 
+void AusgebenGesamt(double einzelpreis)
+{
+	double preisGesamt;
 
-	printf("Anzahl: %d\n", anzahlArtikel);
-	printf("Preis: %.2lf Euro\n", preis);
-
+	printf("Anzahl  Preis gesamt\n");	
+	for(int i = 0; i < 5; i++){
+		printf("%6d", i+1);
+		preisGesamt = einzelpreis * (i+1);
+		printf("%9.2f Euro\n", preisGesamt);
+	}
 	
+}
+int main()
+{
+	meinLogo();
+	/* Mehrfacher Aufruf der Funktion */ 
+	int anzahlFeld[3] = {2, 7, 3};
+	double preisEinzelFeld[3] = {1.45, 0.85, 0.75};
+	int index;
+	double preisGesamt;
+	/* Berechnung und Ausgabe */ 
+	printf("Posten Anzahl Preis einzeln Preis gesamt\n");
+	for(index = 0; index < 3; index++)
+	{
+		preisGesamt = anzahlFeld[index] * preisEinzelFeld[index];
+		printf("%6d %6d %8.2f Euro %7.2f Euro\n", index+1,
+					anzahlFeld[index], preisEinzelFeld[index], preisGesamt);
+	}
 }
